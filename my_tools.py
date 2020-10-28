032b0c27b299158f6aa9fbd792147a7389b97fb2
Onno Doensen <onnodoensen@gmail.com>
2020-10-28 17:40:00 +0100

    28-10-20

--- my_tools.py
+++ my_tools.py
@@ -5,3 +5,23 @@ def convert_date_gefs(actual_time):
     diff = (actual_time-init_time)/ np.timedelta64(1, 'h') 
     return diff
 
+def convert_date_era(actual_time):
+    init_time = pd.Timestamp(1979,1,1)
+    diff = (actual_time-init_time)/ np.timedelta64(1, 'h') 
+    return diff
+    
+def convert_date_gefs_r(hours):
+    init_time = pd.Timestamp(1800,1,1)
+    if (np.shape(hours)!=()):
+        actual_time = [init_time + pd.Timedelta(hour,unit='h') for hour in hours]
+    else:
+        actual_time = init_time + pd.Timedelta(hours,unit='h')
+    return actual_time
+
+def convert_date_era_r(hours):
+    init_time = pd.Timestamp(1979,1,1)
+    if np.shape(hours)!=():
+        actual_time = [init_time + pd.Timedelta(hour,unit='h') for hour in hours]
+    else:
+        actual_time = init_time + pd.Timedelta(hours,unit='h')
+    return actual_time
