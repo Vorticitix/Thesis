a53aecd4e37f5c4248a1b13191495dcc8b750faa
Onno Doensen <onnodoensen@gmail.com>
2020-11-24 20:28:51 +0100

    24-11-20

--- my_tools.py
+++ my_tools.py
@@ -6,7 +6,8 @@ plot_dic = {
     'phasespeed':{'data_variable':'v','title':'Phase Speed','label':'Cp (m/s)','filename':'phasespeed'},
     'u_wind':{'data_variable':'u','title':'Zonal Wind','label':'u (m/s)','filename':'u_wind'},
     'v_wind':{'data_variable':'v','title':'Meridional Wind','label':'v (m/s)','filename':'v_wind'},
-    'groupspeed':{'data_variable':'v','title':'Group Speed','label':'Cg (m/s)','filename':'groupspeed'}
+    'groupspeed':{'data_variable':'v','title':'Group Speed','label':'Cg (m/s)','filename':'groupspeed'},
+    'wavenumber':{'data_variable':'v','title':'Wave Number','label':'k (rad/m)','filename':'wavenumber'}
 }
 
 file_dic = {
@@ -17,7 +18,9 @@ file_dic = {
                'GFS':'gefsrf2_phasevel_wledit2000-10000_latavg_v300_envgt15_control0-252h_6hourly_2x2_dec84-nov19_setvrange_-100to100.nc',
                'CFS':'cfsr-cfsv2_phasevel_wledit2000-10000_latavg_v300_envgt15_79-19_6hourly_anom_from_smoothed04_clim_setvrange_-100to100.nc'},
     'u_wind':{'ERA5':'era5_u300_79-19_6hourly.nc',
-               'GFS':'gefsrf2_u300_control0-252h_6hourly_2x2_dec84-nov19.nc'}
+               'GFS':'gefsrf2_u300_control0-252h_6hourly_2x2_dec84-nov19.nc'},
+    'wavenumber':{'ERA5':'era51_mars_k_wledit2000-10000_latavg_v300_envgt15_79-19_6hourly_setvrange_0to1.nc',
+                  'GFS':'gefsrf2_k_wledit2000-10000_latavg_v300_envgt15_control0-252h_6hourly_2x2_dec84-nov19_setvrange_0to1.nc'}
 }
 #The function below is used to convert real datetimes to hours sicne 1 Jan 1800 because GEFS data are in these units
 def convert_date_gefs(actual_time):
